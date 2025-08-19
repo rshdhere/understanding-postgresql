32a581c28cbca3c41d5086a518645163dee01a9c
raashed <mohammedraashed10@gmail.com>
2025-08-19 19:51:39 +0530

    chore: basic crud using pg-client for postgres

---
+++ src/database/database.ts
@@ -0,0 +1,27 @@
+import "dotenv/config";
+import { Client } from "pg";
+
+const pgClient = new Client({
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  port: parseInt(process.env.DATABASE_PORT || "5432"),
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  ssl: {
+    rejectUnauthorized: false, // For development - use true in production with proper certificates
+  },
+});
+
+export async function ConnectedToDB() {
+  try {
+    await pgClient.connect();
+    console.log("connected to database");
+    // const response = await pgClient.query("SELECT * FROM users;");
+    const response = await pgClient.query(
+      "update users set username='rose mishra' where id='1'",
+    );
+    console.log(response);
+  } catch (error) {
+    console.error("Error occured while connecting to the database", error);
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,17 @@
-console.log("hello from the server");
+import express from "express";
+import { ConnectedToDB } from "./database/database.js";
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+async function StartServer() {
+  await ConnectedToDB();
+  app.listen(PORT, () => {
+    console.log(`your server is listening on http://localhost:${PORT}`);
+  });
+}
+
+StartServer();
