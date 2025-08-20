e7a86b436d27944fe0a6ae982b74805f881667b1
raashed <mohammedraashed10@gmail.com>
2025-08-20 09:57:54 +0530

    refactor: understanding relationships

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -8,9 +8,28 @@ userRouter.post("/sign-up", async (req: Request, res: Response) => {
   const password = req.body.password;
   const email = req.body.email;
 
-  const insertQuery = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3)`;
+  const city = req.body.city;
+  const country = req.body.country;
+  const street = req.body.street;
+  const pincode = req.body.pincode;
+  // RETURNING id must be here
+  const insertQuery = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id`;
+  const response = await pgClient.query(insertQuery, [
+    username,
+    email,
+    password,
+  ]);
+  //perform console.log(userId) to understand
+  const userId = response.rows[0].id;
+  const addressInsertQuery = `INSERT INTO addresses (city, country, street, pincode, user_id) VALUES ($1, $2, $3, $4, $5)`;
 
-  await pgClient.query(insertQuery, [username, email, password]);
+  await pgClient.query(addressInsertQuery, [
+    city,
+    country,
+    street,
+    pincode,
+    userId,
+  ]);
 
   res.json({
     message: "you have signed up",
